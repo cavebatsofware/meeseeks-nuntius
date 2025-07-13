722a26066366c8708d82f5f7ceb921e7067beb7d
Grant DeFayette <grant@cavebatsofware.com>
2025-07-12 21:28:53 -0400

    remove unneeded pub use for message

--- api/src/pgp_mgr/mod.rs
+++ api/src/pgp_mgr/mod.rs
@@ -3,17 +3,3 @@ pub mod generate;
 
 #[cfg(test)]
 mod test_generate;
-
-/* pub use message::{
-    EncryptedMessage,
-    SignedMessage,
-    DecryptedMessage,
-    MessageError,
-    encrypt_message,
-    sign_message,
-    encrypt_and_sign_message,
-    decrypt_message,
-    verify_signed_message,
-    create_detached_signature,
-    verify_detached_signature,
-}; */
